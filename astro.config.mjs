f6f92430bb96bbdc19bf3b429eac404a8572684f
IvanRamirezz <92224804+IvanRamirezz@users.noreply.github.com>
2025-03-20 08:54:11 -0600

    Add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,19 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+
+
 // https://astro.build/config
 export default defineConfig({
+  output: "static", // Importante para GitHub Pages
+  base: "/WebActi/", // ⚠️ Cambia "WebActi" por el nombre de tu repo en GitHub
+  site: "https://ivanramirezz.github.io/WebActi/", // ⚠️ Cambia esto por tu URL de GitHub Pages
+
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
   }
+
+
 });
